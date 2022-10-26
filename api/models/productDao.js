52866fcb36c181722fcfe888167a36f8dd1c2df5
이찬우 <scflcweric1201@gmail.com>
2022-10-26 22:30:26 +0900

    [Modify] productDao sub query문으로 바꿈

--- api/models/productDao.js
+++ api/models/productDao.js
@@ -2,44 +2,47 @@ const { wekeaDataSource } = require('./dataSource');
 
 const getProductDetailById = async(id) => {
     const products = await wekeaDataSource.query(`
-        SELECT
-            products.id AS id,
-            products.name AS name,
-            products.thumbnail AS thumbnail,
-            products.description AS description,
-            categories.name AS category
-        FROM products
-        INNER JOIN categories ON products.category_id=categories.id
-        WHERE products.id=?;`,[id]
-    );
-     const productImages = await wekeaDataSource.query(`
-        SELECT
-          id,
-          image_url AS url
-        FROM images
-        WHERE product_id=?;`,[id]
-     );
-     const productOptions = await wekeaDataSource.query(`
-        SELECT
-          size,
-          price
-        FROM product_options
-        WHERE product_id=?;`, [id]
-     );
-     const productOptionColor = await wekeaDataSource.query(`
-        SELECT
-          color
-        FROM product_options
-        WHERE product_id=?;`, [id]
-     );
-
-     productOptions[0]['color']=[];
-     productOptions[0].color.push(productOptionColor[0].color, productOptionColor[1].color);
-
-     products[0]['images']=productImages;
-     products[0]['options']=productOptions[0];
-
-     return products[0];
+      SELECT
+          products.id AS id,
+          products.name AS name,
+          products.thumbnail AS thumbnail,
+          products.description AS description,
+          categories.name AS category,
+          pi.images,
+          po.size,
+          po.price,
+          po.idAndColor
+    FROM products
+    INNER JOIN categories ON products.category_id=categories.id
+    LEFT JOIN (
+          SELECT
+              product_id,
+              JSON_ARRAYAGG(
+                    JSON_OBJECT(
+                        "id", id,
+                        "url", image_url
+                    ) 
+              ) as images
+              FROM images
+              GROUP BY product_id
+    ) pi ON products.id = pi.product_id
+    LEFT JOIN(
+          SELECT
+              product_id,
+              size,
+              price,
+              JSON_ARRAYAGG(
+                    JSON_OBJECT(
+                        "productOptionId", id,
+                        "color", color
+                    )
+              ) as idAndColor
+          FROM product_options
+          GROUP BY product_id, size, price
+    ) po ON products.id=po.product_id
+    WHERE products.id=?
+    GROUP BY products.id, po.size, po.price`, [id]);
+    return products[0];
 };
 
 module.exports = {
